5e32509f67c5b183893a73bd82d0faa0ee6be713
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-11-18 16:27:33 +0100

    [scenario_state_base] Removed the 'knowledge_storing_enabled' attribute

--- ros/src/mas_execution_manager/scenario_state_base.py
+++ ros/src/mas_execution_manager/scenario_state_base.py
@@ -22,7 +22,6 @@ class ScenarioStateBase(smach.State):
         self.retry_count = 0
         self.executing = False
         self.succeeded = False
-        self.knowledge_storing_enabled = rospy.get_param('/store_knowledge', False)
         self.say_pub = rospy.Publisher('/say', String, latch=True, queue_size=1)
 
         self.action_dispatch_pub = rospy.Publisher('/kcl_rosplan/action_dispatch',
@@ -51,18 +50,17 @@ class ScenarioStateBase(smach.State):
                         self.robot_name = param.value
                         break
                 break
-        except (rospy.ServiceException, rospy.ROSException), exc:
+        except (rospy.ServiceException, rospy.ROSException) as exc:
             rospy.logwarn('Service /rosplan_knowledge_base/state/propositions does not appear to exist.\n' +
                           'If you intend to use the knowledge base, please spawn ' +
                           'rosplan_knowledge_base/knowledgeBase')
 
-        if self.knowledge_storing_enabled:
-            try:
-                self.msg_store_client = MessageStoreProxy()
-            except:
-                rospy.logwarn('Could not create a mongodb_store proxy.\n' +
-                              'If you intend to store online knowledge, please spawn\n' +
-                              'mongodb_store/message_store_node.py')
+        try:
+            self.msg_store_client = MessageStoreProxy()
+        except:
+            rospy.logwarn('Could not create a mongodb_store proxy.\n' +
+                          'If you intend to store online knowledge, please spawn\n' +
+                          'mongodb_store/message_store_node.py')
 
     def execute(self, userdata):
         pass
