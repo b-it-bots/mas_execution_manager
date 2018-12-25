5a1902c61328bfca4c4bd21eef796c34be48187a
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-12-26 00:15:20 +0100

    Modified the scenario state base so that it uses the KB interface in mas_knowledge_base

--- ros/src/mas_execution_manager/scenario_state_base.py
+++ ros/src/mas_execution_manager/scenario_state_base.py
@@ -1,12 +1,10 @@
 import rospy
 import smach
 from std_msgs.msg import String
-
 import rosplan_dispatch_msgs.msg as plan_dispatch_msgs
-import rosplan_knowledge_msgs.srv as rosplan_srvs
-from mongodb_store.message_store import MessageStoreProxy
 
 from mdr_monitoring_msgs.msg import ExecutionState
+from mas_knowledge_base.domestic_kb_interface import DomesticKBInterface
 
 class ScenarioStateBase(smach.State):
     def __init__(self, action_name, outcomes,
@@ -32,35 +30,8 @@ class ScenarioStateBase(smach.State):
                          plan_dispatch_msgs.ActionFeedback,
                          self.get_action_feedback)
 
-        self.robot_name = ''
-        self.attribute_fetching_client = None
-        self.msg_store_client = None
-
-        try:
-            rospy.wait_for_service('/rosplan_knowledge_base/state/propositions', 5.)
-            self.attribute_fetching_client = rospy.ServiceProxy('/rosplan_knowledge_base/state/propositions',
-                                                                rosplan_srvs.GetAttributeService)
-            self.robot_name = ''
-            request = rosplan_srvs.GetAttributeServiceRequest()
-            request.predicate_name = 'robot_name'
-            result = self.attribute_fetching_client(request)
-            for item in result.attributes:
-                for param in item.values:
-                    if param.key == 'bot':
-                        self.robot_name = param.value
-                        break
-                break
-        except (rospy.ServiceException, rospy.ROSException) as exc:
-            rospy.logwarn('Service /rosplan_knowledge_base/state/propositions does not appear to exist.\n' +
-                          'If you intend to use the knowledge base, please spawn ' +
-                          'rosplan_knowledge_base/knowledgeBase')
-
-        try:
-            self.msg_store_client = MessageStoreProxy()
-        except:
-            rospy.logwarn('Could not create a mongodb_store proxy.\n' +
-                          'If you intend to store online knowledge, please spawn\n' +
-                          'mongodb_store/message_store_node.py')
+        self.kb_interface = DomesticKBInterface()
+        self.robot_name = self.kb_interface.robot_name
 
     def execute(self, userdata):
         pass
@@ -70,10 +41,7 @@ class ScenarioStateBase(smach.State):
         execution_state_msg.stamp = rospy.Time.now()
         execution_state_msg.state_machine = self.sm_id
         execution_state_msg.state = self.state_name
-        try:
-            self.msg_store_client.insert_named('current_state', execution_state_msg)
-        except:
-            rospy.logerr('Error while saving current state')
+        self.kb_interface.insert_obj_instance('current_state', execution_state_msg)
 
     def get_dispatch_msg(self):
         pass
