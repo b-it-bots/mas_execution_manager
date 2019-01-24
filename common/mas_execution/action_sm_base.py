992df82107c250ae3f9574abb49af7e8f430a619
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-01-24 17:42:30 +0100

    Added a base class for implementing actions

---
+++ common/mas_execution/action_sm_base.py
@@ -0,0 +1,33 @@
+from abc import abstractmethod
+from pyftsm.ftsm import FTSM, FTSMTransitions
+
+class ActionSMBase(FTSM):
+    def __init__(self, name, dependencies, max_recovery_attempts=1):
+        super(ActionSMBase, self).__init__(name, dependencies, max_recovery_attempts)
+        self.execution_requested = False
+        self.goal = None
+        self.result = None
+
+    def init(self):
+        return FTSMTransitions.INITIALISED
+
+    def configuring(self):
+        return FTSMTransitions.DONE_CONFIGURING
+
+    def ready(self):
+        if self.execution_requested:
+            self.result = None
+            self.execution_requested = False
+            return FTSMTransitions.RUN
+        else:
+            return FTSMTransitions.WAIT
+
+    def running(self):
+        return FTSMTransitions.DONE
+
+    def recovering(self):
+        return FTSMTransitions.DONE_RECOVERING
+
+    @abstractmethod
+    def set_result(self, success):
+        pass
--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['mas_execution_manager'],
-    package_dir={'mas_execution_manager': 'ros/src/mas_execution_manager'}
+    packages=['mas_execution', 'mas_execution_manager'],
+    package_dir={
+        'mas_execution': 'common/mas_execution',
+        'mas_execution_manager': 'ros/src/mas_execution_manager'
+    }
 )
 
 setup(**d)
