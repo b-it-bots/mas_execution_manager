2b7aa344f4266bae9b38c6d3d12596a676cb67ad
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-02-11 22:55:18 +0100

    [action_sm_base] Added an execute fn for abstracting away the logic of calling an action server

--- common/mas_execution/action_sm_base.py
+++ common/mas_execution/action_sm_base.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import time
 from pyftsm.ftsm import FTSM, FTSMTransitions
 
 class ActionSMBase(FTSM):
@@ -30,6 +31,24 @@ class ActionSMBase(FTSM):
 
     def recovering(self):
         return FTSMTransitions.DONE_RECOVERING
+
+    def execute(self, action_server, goal):
+        '''Sends the given goal to the action server and waits for a result from
+        the server. Sets self.preempted to True if the action is preempted.
+
+        Keyword arguments:
+        action_server: actionlib.SimpleActionServer
+        goal -- action goal
+
+        '''
+        self.goal = goal
+        self.result = None
+        self.execution_requested = True
+        while not self.result:
+            if action_server.is_preempt_requested():
+                self.preempted = True
+            time.sleep(0.05)
+        action_server.set_succeeded(self.result)
 
     @abstractmethod
     def set_result(self, success):
